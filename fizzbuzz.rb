88481b095e28ef13954021a9a9a49162e837ab0d
taroj khan <tarujkhan@yahoo.com>
2020-08-19 19:57:12 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,27 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+# def fizzbuzz(number)
+#   if number % 3 == 0
+#     "Fizz"
+# elsif number % 5 == 0
+#     "Buzz"
+# #elsif number % 3 == 0 && number % 5 == 0
+# elsif number % 15 == 0
+#     "FizzBuzz"
+# else 
+#   puts nil
+# end
+#end
+
+def fizzbuzz(number)
+  if number % 15 == 0 
+    "FizzBuzz"
+  elsif number % 3 == 0 
+    "Fizz"
+  elsif number % 5 == 0 
+    "Buzz"
+  else 
+    nil
+  end
+end
