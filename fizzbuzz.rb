8083759412a0635ffe05ea73174d3df4e79a411e
taroj khan <tarujkhan@yahoo.com>
2020-08-19 19:55:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -13,3 +13,15 @@ else
   puts nil
 end
 end
+
+def fizzbuzz(number)
+  if number % 15 == 0 
+    "FizzBuzz"
+  elsif number % 3 == 0 
+    "Fizz"
+  elsif number % 5 == 0 
+    "Buzz"
+  else 
+    nil
+  end
+end
