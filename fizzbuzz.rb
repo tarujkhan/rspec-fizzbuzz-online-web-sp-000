9910039c0fea3768e2fac7f2d813ec3f0d0538f7
taroj khan <tarujkhan@yahoo.com>
2020-08-19 19:17:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,9 +6,7 @@ def fizzbuzz(number)
     "Fizz"
 elsif number % 5 == 0
     "Buzz"
-elsif number % 3 && number % 5
-#number % 15 == 0
-#number % 3 == 0 && number % 5 == 0
+elsif number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
 else 
   puts nil
